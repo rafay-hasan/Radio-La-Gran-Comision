3585bc4874a2e9849be0d92e5ff086154b46193a
Rafay Hasan <rafayhasan@rafays-macbook-pro-2.local>
2016-11-16 00:37:56 +0600

    aaa

--- RADIO LA GRAN COMISIÓN/ViewController.m
+++ RADIO LA GRAN COMISIÓN/ViewController.m
@@ -9,11 +9,26 @@
 #import "ViewController.h"
 #import "StreamingFile.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+{
+    NSInteger imageCounter;
+}
+
+
 
 @property (weak, nonatomic) IBOutlet UIButton *radioButton;
 
 @property (weak, nonatomic) IBOutlet UILabel *songNameLabel;
+- (IBAction)galleryAction:(id)sender;
+
+- (IBAction)worldIconAction:(id)sender;
+
+- (IBAction)facebookAction:(id)sender;
+- (IBAction)twitterAction:(id)sender;
+
+- (IBAction)buyMusicAction:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *homeBgImageView;
 
 @end
 
@@ -23,7 +38,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    //NSTimer *aTimer = [NSTimer scheduledTimerWithTimeInterval:10 target:self selector:@selector(showShongData) userInfo:nil repeats:YES];
+    imageCounter = 0;
+    
+    NSTimer *aTimer = [NSTimer scheduledTimerWithTimeInterval:5 target:self selector:@selector(changeImage) userInfo:nil repeats:YES];
     
     //[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(songChanged:) name:<#(nullable NSNotificationName)#> object:<#(nullable id)#>
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(triggerAction:) name:@"SongNotification" object:nil];
@@ -61,4 +78,54 @@
 }
 
 
+- (IBAction)galleryAction:(id)sender {
+    
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc]init];
+    imagePickerController.delegate = self;
+    imagePickerController.sourceType =  UIImagePickerControllerSourceTypePhotoLibrary;
+    [self presentViewController:imagePickerController animated:YES completion:nil];}
+
+
+- (IBAction)worldIconAction:(id)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"http://radiolagrancomisiontv.org"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+
+}
+
+- (IBAction)facebookAction:(id)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"https://www.facebook.com/lagrancomisionradio/"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+
+}
+
+- (IBAction)twitterAction:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"https://twitter.com/RLaGranComision"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+- (IBAction)buyMusicAction:(id)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"http://tunein.com/radio/Radio-La-Gran-Comision-s207440/"];
+    [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
+}
+
+-(void) changeImage
+{
+    imageCounter = imageCounter + 1;
+    self.homeBgImageView.image = [UIImage imageNamed:[NSString stringWithFormat:@"%li",(long)imageCounter]];
+    
+    if(imageCounter == 4)
+        imageCounter = 0;
+    
+}
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+     [picker dismissViewControllerAnimated:YES completion:nil];}
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
 @end
